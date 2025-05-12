2c51d80384621f6962543c8086f671a651949cbf
Olga <minaeva9@gmail.com>
2025-05-12 18:57:07 -0400

    cart adjust position

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,8 +4,11 @@ import { createPortal } from "react-dom";
 function Cart() {
  
  return createPortal(
-   <div className="w-[320px] bg-red-600 rounded-lg shadow-lg p-4">
-     <h1>i am cart</h1>
+   <div className="w-[320px] rounded-lg shadow-lg p-4  fixed top-20 right-20 flex flex-col">
+   <h1>Cart</h1>
+   <div>
+    product
+   </div>
   </div>,
   document.body
  );
