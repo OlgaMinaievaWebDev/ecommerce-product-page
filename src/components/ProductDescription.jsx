a687f5901fb732eaf3e91615ed8ae73855b4f03c
Olga <minaeva9@gmail.com>
2025-05-17 15:11:39 -0400

    description text larger

--- src/components/ProductDescription.jsx
+++ src/components/ProductDescription.jsx
@@ -6,17 +6,17 @@ function ProductDescription() {
   const firstProduct = product[0]; // access the first product
 
   return (
-    <section className="space-y-4 w-[350px]">
+    <section className="space-y-4 w-[450px]">
       <h3 className="uppercase font-semibold text-grayishBlue tracking-wide">
         Sneaker Company
       </h3>
 
-      <h1 className="text-3xl font-black leading-tight">{firstProduct.name}</h1>
+      <h1 className="text-4xl font-black leading-tight">{firstProduct.name}</h1>
 
-      <p className="text-md text-grayishBlue">{firstProduct.description}</p>
+      <p className="text-lg text-grayishBlue">{firstProduct.description}</p>
 
       <div className="flex items-center space-x-4">
-        <p className="font-black text-lg">${firstProduct.price}</p>
+        <p className="font-black text-2xl">${firstProduct.price}</p>
         <span className="rounded-md bg-black text-white py-1 px-2 text-sm">
           {firstProduct.discount}%
         </span>
